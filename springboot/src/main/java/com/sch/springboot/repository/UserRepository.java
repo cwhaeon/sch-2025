11be198ed6a1f8c72402f090dcd0953dcf4d019c
cwhaeon <1122334cw@gmail.com>
2025-08-27 17:17:10 +0900

    a

--- springboot/src/main/java/com/sch/springboot/repository/UserRepository.java
+++ springboot/src/main/java/com/sch/springboot/repository/UserRepository.java
@@ -8,44 +8,41 @@ import java.util.Map;
 
 @Repository
 public class UserRepository {
-    private final UserService userService;
-
-    public UserRepository(UserService userService) {
-        this.userService = userService;
-    }
-
     /**
-     * signUp: 회원가입 처리
+     * signUp : 회원가입 처리
      */
-    public Map<String,String> signUp(User user){
-        Map<String,String> result = new HashMap<>();
-        return userService;
+    public Map<String, String> signUp(User user) {
+        Map<String, String> result = new HashMap<>();
+
+        result.put("id", user.getId());
+        result.put("pass", user.getPass());
+        result.put("name", user.getName());
+        result.put("email", user.getEmail());
+
+        return result;
     }
-}
 
 
-@Repository
-public class UserRepository {
     /**
-     * login: 로그인 처리
+     * login : 로그인 처리
      */
     public Map<String, Object> login(User user) {
         String did = "test";
         String dpass = "1234";
         Map<String, Object> result = new HashMap<String, Object>();
 
-        if (user.getId().equals(did) && user.getPass().equals(dpass)) {
-            // 로그인 성공
+        if(user.getId().equals(did) && user.getPass().equals(dpass)){
+            //로그인 성공
             result.put("status", "success");
-            result.put(("massage"), "로그인 성공!");
+            result.put("message", "로그인 성공!");
             result.put("id", user.getId());
-        }
-        else {
-            // 로그인 실패
+        } else {
+            //로그인 실패
             result.put("status", "fail");
-            result.put(("massage"), "로그인 실패!");
+            result.put("message", "로그인 실패!");
             result.put("id", user.getId());
         }
         return result;
     }
+
 }
--- springboot/src/main/java/com/sch/springboot/service/EmployeeService.java
+++ springboot/src/main/java/com/sch/springboot/service/EmployeeService.java
@@ -1,4 +1,30 @@
 package com.sch.springboot.service;
 
-public class EmplyeeService {
+import com.sch.springboot.dto.Employee;
+import com.sch.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    //생성자를 이용하여 EmployeeRepository를 Loose -> DI
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    //사원리스트
+    public List<Employee> findAll(){
+        return employeeRepository.selectAll();
+    }
+
+    //사원등록
+    public String register(Employee employee) {
+        return employeeRepository.insert(employee);
+    }
 }
